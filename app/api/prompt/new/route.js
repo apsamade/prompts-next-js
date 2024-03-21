37eed23a567ac460a1c8fd4a79086cab99b0eed4
apsamade <bouderga.abdessamade@gmail.com>
2024-03-21 20:23:06 +0100

    maj

--- app/api/prompt/new/route.js
+++ app/api/prompt/new/route.js
@@ -1,15 +1,15 @@
 import { connectToDB } from "@utils/database"
 import Prompt from "@models/prompt";
 
-export const POST = async (request)=> {
-    const { userId, prompt, tag } = await request.json()
+export const POST = async (req, res)=> {
+    const { userId, prompt, tag } = await req.json()
 
     try {
         await connectToDB();
         const newPrompt = new Prompt({ creator: userId, prompt, tag });
 
         await newPrompt.save()
-        return new Response(JSON.strongify(newPrompt), { status: 201 })
+        return new Response(JSON.stringify(newPrompt), { status: 201 })
     } catch (error) {
         return new Response("Échec lors de la création du prompt.", { status : 500 })
     }
--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,10 +1,61 @@
-import React from 'react'
+'use client'
+
+import { useState, useEffect } from "react"
+
+import PromptCard from "./PromptCard"
+
+const PromptCardList = ({data, handleTagClick}) => {
+    return(
+        <div className="mt-16 prompt_layout">
+            {data.map((post) => (
+                <PromptCard 
+                    key={post._id}
+                    post={post}
+                    handleTagClick={handleTagClick}
+                />
+            ))}
+        </div>
+    )
+}
 
 const Feed = () => {
+
+    const [searchText, setSearchText] = useState('')
+    const [post, setPost] = useState([])
+
+    const handleSearchChange = (e) => {
+
+    }
+
+    useEffect(()=> {
+        const fetchPosts = async () => {
+            const response = await fetch('/api/prompt')
+            const data = await response.json()
+
+            setPost(data)
+        }
+        
+        fetchPosts();
+        
+    }, []);
     return (
-        <div>
-            Feed
-        </div>
+        <section className="feed">
+            <form className="relative w-full flex-center">
+                <input 
+                    type="text" 
+                    placeholder="Recherche pour un tag ou un pseudo"
+                    value={searchText}
+                    onChange={handleSearchChange}
+                    required
+                    className="search_input peer"
+                />
+            </form>
+
+            <PromptCardList 
+                data={post}
+                handleTagClick={() => {}}
+            />
+        </section>
     )
 }
 
